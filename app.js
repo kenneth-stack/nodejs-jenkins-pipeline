ab71508f16e565a3887f86cf9f74cfd45f6683a7
kenneth-stack <kennethfavour93@gmail.com>
2024-08-29 11:48:15 +0100

    jenkins pipeline

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const hostname = '127.0.0.1'; // Your server ip address
+const hostname = '18.223.209.120'; // Your server ip address
 const port = 5000;
 
 const version = '1.0.0';
